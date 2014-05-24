8468570b2ea98a99866003972bb5442eaa55fc7a
Ichinose Shogo <shogo82148@gmail.com>
2014-05-24 12:07:54 +0900

    init commit

---
+++ MiniMessagePack/MiniMessagePacker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniMessagePack
+{
+	public class MiniMessagePacker
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ MiniMessagePackTest/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MiniMessagePackTest
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
